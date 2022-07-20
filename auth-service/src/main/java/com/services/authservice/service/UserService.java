3ba4d5fcbef77b614cfe5c7c202dfabd4c7426ae
pielena <94699762+pielena@users.noreply.github.com>
2022-07-20 17:19:54 +0300

    Revert "Feature/asa 10"

--- auth-service/src/main/java/com/services/authservice/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.services.authservice.service;
-
-public interface UserService {
-
-    void register(String username, String clientSecret);
-
-    void checkCredentials(String username, String clientSecret);
-}
